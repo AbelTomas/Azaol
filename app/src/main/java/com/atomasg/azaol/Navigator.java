74e61403d2fc75ee2cca21efd5b9e81154e30e41
Abel <>
2018-10-22 20:35:23 +0200

    added navigation method and animations to menu activity

--- app/src/main/java/com/atomasg/azaol/Navigator.java
+++ app/src/main/java/com/atomasg/azaol/Navigator.java
@@ -3,6 +3,7 @@ package com.atomasg.azaol;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 public class Navigator {
 
@@ -42,12 +43,20 @@ public class Navigator {
        // Intent intert = new Intent(activity, RegisterList.class);
        // activity.startActivity(intert);
 
+        Toast.makeText(activity,"Go to List Intent",Toast.LENGTH_SHORT).show();
+
     }
 
     protected void goToLogin() {
         Log.d(TAG,"Going to Login...");
         //   Intent intert = new Intent(Navigation.this, LoginActivity.class);
         //   startActivity(intert);
+
+        Toast.makeText(activity,"Go to Login Intent",Toast.LENGTH_SHORT).show();
+    }
+
+    public void goToMap() {
+        Toast.makeText(activity,"Go to Map Intent",Toast.LENGTH_SHORT).show();
     }
 }
 
